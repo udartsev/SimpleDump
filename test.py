d21f30fb6fbd33b2365db49d01d5294ad3d53689
Vladimir Udartsev <vladimir.udartsev@appcraft.team>
2019-09-05 14:33:51 +0300

    added test file

---
+++ test.py
@@ -0,0 +1,13 @@
+from SimpleDump import var_dump, dd, dump
+
+test1 = "TESTSTSTSTS"
+test2 = True
+test3 = 1
+test4 = [1, 2, 3, 4, 5]
+test5 = {}
+
+dump(test1)
+dump(test2)
+dump(test3)
+dump(test4)
+dd(test5)
